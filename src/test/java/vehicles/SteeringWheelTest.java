6fd8b63c2f24aae1012440de68b86c2b67817258
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-15 14:15:35 +0000

    created test for SteeringWheel class

--- src/test/java/vehicles/SteeringWheelTest.java
+++ src/test/java/vehicles/SteeringWheelTest.java
@@ -1,4 +1,22 @@
 package vehicles;
 
+import org.junit.Before;
+import org.junit.Test;
+import vehicles.parts.SteeringWheel;
+
+import static org.junit.Assert.assertEquals;
+
 public class SteeringWheelTest {
+
+    SteeringWheel steeringWheel;
+
+    @Before
+    public void before(){
+        steeringWheel = new SteeringWheel();
+    }
+
+    @Test
+    public void canBeepHorn(){
+        assertEquals("Beep beep!", steeringWheel.tootHorn());
+    }
 }
