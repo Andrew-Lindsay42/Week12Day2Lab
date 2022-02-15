12a8d53a75d3db18d01e1f2648f47e04108bf249
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-15 14:05:32 +0000

    Created tests for Engine Class

--- src/test/java/vehicles/EngineTest.java
+++ src/test/java/vehicles/EngineTest.java
@@ -1,4 +1,36 @@
 package vehicles;
 
+import org.junit.Before;
+import org.junit.Test;
+import vehicles.parts.Engine;
+
+import static org.junit.Assert.assertEquals;
+
 public class EngineTest {
+    Engine engine;
+
+    @Before
+    public void before(){
+        engine = new Engine(4.2, 500);
+    }
+
+    @Test
+    public void hasSize(){
+        assertEquals(4.2, engine.getSize());
+    }
+
+    @Test
+    public void hasBHP(){
+        assertEquals(500, engine.getHorsepower());
+    }
+
+    @Test
+    public void canStart(){
+        assertEquals("Vroom", engine.start());
+    }
+
+    @Test
+    public void canStop(){
+        assertEquals("---", engine.stop());
+    }
 }
