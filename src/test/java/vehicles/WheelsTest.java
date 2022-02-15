90cf69da590e551e75e9b88e317334476eb22f14
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-15 14:11:29 +0000

    Created tests for Wheel Class

--- src/test/java/vehicles/WheelsTest.java
+++ src/test/java/vehicles/WheelsTest.java
@@ -1,4 +1,27 @@
 package vehicles;
 
+import org.junit.Before;
+import org.junit.Test;
+import vehicles.parts.Wheels;
+
+import static org.junit.Assert.assertEquals;
+
 public class WheelsTest {
+
+    Wheels wheels;
+
+    @Before
+    public void before(){
+        wheels = new Wheels("Continental", 24);
+    }
+
+    @Test
+    public void hasManufacturer(){
+        assertEquals("Continental", wheels.getTyreManufacturer());
+    }
+
+    @Test
+    public void hasSize(){
+        assertEquals(24, wheels.getSize());
+    }
 }
