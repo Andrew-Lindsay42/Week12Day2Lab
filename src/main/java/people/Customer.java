8187d97dd98f655c17d7e4b30a9afc95504fe34d
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-15 15:06:46 +0000

    Created Customer Class

--- src/main/java/people/Customer.java
+++ src/main/java/people/Customer.java
@@ -1,4 +1,41 @@
 package people;
 
+import vehicles.Car;
+
+import java.util.ArrayList;
+
 public class Customer {
+    private String name;
+    private double money;
+    private ArrayList<Car> collection;
+
+    public Customer(String name, double money) {
+        this.name = name;
+        this.money = money;
+        this.collection = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public int countCars() {
+        return collection.size();
+    }
+
+    public void buyCar(Car car) {
+        collection.add(car);
+    }
+
+    public void sellCar(Car car) {
+        collection.remove(car);
+    }
 }
